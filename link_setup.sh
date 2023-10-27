c5f5c114b7818f2d2bf83346a4a65a238706582b
MxDx <maxime.dxl2002@gmail.com>
2023-10-27 15:26:39 +0200

    setup script

---
+++ link_setup.sh
@@ -0,0 +1,7 @@
+rm ~/.zshrc
+rm ~/.p10k.zsh
+rm -rf ~/.config/nvim 
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -s ~/dotfiles/nvim ~/.config/nvim
--- setup.sh
+++ setup.sh
@@ -2,10 +2,5 @@ sudo apt-get update
 sudo apt-get install zsh
 chsh -s /bin/zsh
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 sudo apt install fonts-firacode
-
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
-ln -s ~/dotfiles/nvim ~/.config/nvim
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
