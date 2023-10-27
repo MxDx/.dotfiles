f715ae603d59afa00279f50ee87ae4eff88b4961
MxDx <maxime.dxl2002@gmail.com>
2023-10-27 15:36:15 +0200

    clone plugins

--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@ sudo apt-get install zsh
 chsh -s /bin/zsh
 
 sudo apt install fonts-firacode
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# ZSH CUSTOM
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zpm-zsh/ls.git ${ZSH_CUSTOM1:-~/.oh-my-zsh/custom}/plugins/ls
+
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
