f2a6c82e325115152959ada618e7d1dfd21d9f02
MxDx <maxime.dxl2002@gmail.com>
2024-01-08 10:41:53 +0100

    Setup arch

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,18 +1,19 @@
-sudo apt-get update
-sudo apt-get install zsh
+sudo pacman -S zsh
 chsh -s /bin/zsh
 
-# NVIM  
-sudo apt-get install neovim
+# Installing the font
+yay -Sy --noconfirm ttf-meslo-nerd-font-powerlevel10k
 
 # POWERLEVEL10K
-sudo apt install fonts-firacode
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# oh-my-zsh
+yay -Sy --noconfirm ttf-meslo-nerd-font-powerlevel10k
 
 # ZSH CUSTOM
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zpm-zsh/ls.git ${ZSH_CUSTOM1:-~/.oh-my-zsh/custom}/plugins/ls
-sudo apt-get install exa
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Exa for ls 
+yay -S exa
---
+++ setup_deb.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo apt-get install zsh
+chsh -s /bin/zsh
+
+# NVIM  
+sudo apt-get install neovim
+
+# POWERLEVEL10K
+sudo apt install fonts-firacode
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# ZSH CUSTOM
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zpm-zsh/ls.git ${ZSH_CUSTOM1:-~/.oh-my-zsh/custom}/plugins/ls
+sudo apt-get install exa
+
+
