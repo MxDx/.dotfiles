949510469cfcd48dd12a2dc74bbfd4c7138108c1
MxDx <maxime.dxl2002@gmail.com>
2024-01-08 10:45:24 +0100

    git config global

--- link_setup.sh
+++ link_setup.sh
@@ -5,3 +5,6 @@ rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.dotfiles/nvim ~/.config/nvim
+
+git config --global user.name 'MxDx'
+git config --global user.emai 'maxime.dxl2002@gmail.com'
