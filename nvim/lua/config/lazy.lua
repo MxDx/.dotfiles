f6bc5e04813ddc6fd60d34d48a42e6ab557e125a
MxDx <maxime.dxl2002@gmail.com>
2024-01-17 17:34:09 +0100

    adding auto save

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -4,6 +4,7 @@ if not vim.loop.fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
@@ -13,7 +14,7 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
