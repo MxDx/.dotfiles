7aee13c59e04bd1c2d48d699b95200fa64e8551f
MxDx <maxime.dxl2002@gmail.com>
2025-04-20 15:44:32 +0200

    noice

--- nvim/.config/nvim/lua/plugins/basics.lua
+++ nvim/.config/nvim/lua/plugins/basics.lua
@@ -7,26 +7,6 @@ return {
             vim.cmd.colorscheme("catppuccin")
         end,
     },
-    -- Beautiful ui select
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            -- This is your opts table
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({
-                            -- even more opts
-                        }),
-                    },
-                },
-            })
-            -- To get ui-select loaded and working with telescope, you need to call
-            -- load_extension, somewhere after setup function:
-            require("telescope").load_extension("ui-select")
-        end,
-    },
-
     {
         -- For syntax highlighting
         "nvim-treesitter/nvim-treesitter",
